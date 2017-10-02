644bf19a7ecd0fe36b1912a412321a298f2eecd9
wilqor <wilqor@gmail.com>
2017-10-03 01:18:48 +0200

    Add Hamcrest matcher examples and exercises

---
+++ src/test/java/com/workshop/wilqor/testing/matchers/examples/MatchersExampleTest.java
@@ -0,0 +1,73 @@
+package com.workshop.wilqor.testing.matchers.examples;
+
+import com.workshop.wilqor.testing.Point;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Example demonstrating usage of Hamcrest matchers.
+ *
+ * @author wilqor
+ */
+public class MatchersExampleTest {
+    @Test
+    public void assertTruth_twoPlusTwoIsFour() {
+        // there is no negative alternative
+        MatcherAssert.assertThat("2 plus 2 should be equal to 4", 2 + 2 == 4);
+    }
+
+    @Test
+    public void assertEquality_integers() {
+        // psst, see what happens when you add static imports!
+        MatcherAssert.assertThat("2 plus 2 should be equal to 4", 2 + 2, Matchers.is(Matchers.equalTo(4)));
+    }
+
+    @Test
+    public void assertEquality_strings() {
+        // by the way, the "equalTo" part is optional, but looks better with static imports
+        MatcherAssert.assertThat("concatenated string parts should be equal to the concatenation",
+                "a" + "bc", Matchers.is(Matchers.equalTo("abc")));
+    }
+
+    @Test
+    public void assertEquality_doublePrecisionFloatingPoints() {
+        // note: only double precision floating point is supported
+        MatcherAssert.assertThat(1.0d + 0.009, Matchers.is(Matchers.closeTo(1.01d, 0.01d)));
+    }
+
+    @Test
+    public void assertEquality_objects() {
+        Point point = new Point(10, 20);
+        Point pointWithSameCoordinates = new Point(15 - 5, 15 + 5);
+        MatcherAssert.assertThat(pointWithSameCoordinates, Matchers.is(Matchers.equalTo(point)));
+    }
+
+    @Test
+    public void assertNullValue_noValueForEmptyMap() {
+        // empty map has no entries, so for each key we expect null
+        Map<Integer, String> map = Collections.emptyMap();
+        MatcherAssert.assertThat(map.get(10), Matchers.is(Matchers.nullValue()));
+    }
+
+    @Test
+    public void assertAnyOf_integerOutsideExclusiveRange() {
+        // at least one condition has to be true
+        MatcherAssert.assertThat(150, Matchers.anyOf(
+                Matchers.lessThan(50),
+                Matchers.greaterThan(100)
+        ));
+    }
+
+    @Test
+    public void testAllOf_integerInsideRangeInclusive() {
+        // each condition has to be true
+        MatcherAssert.assertThat(150, Matchers.anyOf(
+                Matchers.greaterThanOrEqualTo(100),
+                Matchers.lessThanOrEqualTo(150)
+        ));
+    }
+}
---
+++ src/test/java/com/workshop/wilqor/testing/matchers/exercises/MatchersExerciseTest.java
@@ -0,0 +1,64 @@
+package com.workshop.wilqor.testing.matchers.exercises;
+
+import com.workshop.wilqor.testing.Point;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Exercise objective: Use Hamcrest matchers in defined tests
+ * to check what is described by the method name.
+ *
+ * @author wilqor
+ */
+public class MatchersExerciseTest {
+    @Test
+    public void assertInequality_pointsWithDifferentCoordinatesAreNotEqual() {
+        Point pointA = new Point(10, 15);
+        Point pointB = new Point(15, 10);
+        // place for your assertion using MatcherAssert.assertThat and proper matcher from Matchers.
+        // hint: almost the same like asserting equality
+    }
+
+    @Test
+    public void assertNotNullValue_valueForKeyOneIsNotNull() {
+        Map<Integer, String> map = Collections.singletonMap(1, "some value");
+        String value = map.get(1);
+        // place for your assertion using MatcherAssert.assertThat and proper matcher from Matchers.
+        // hint: almost the same like asserting null value
+    }
+
+    @Test
+    public void assertStringEqualityIgnoringCase_differentCaseStrings() {
+        String lowerCase = "lowercasestring";
+        String upperCase = "UPPERCASESTRING";
+        // place for your assertion using MatcherAssert.assertThat and proper matcher from Matchers.
+    }
+
+    @Test
+    public void assertStringContainsSubstringInTheMiddle() {
+        String container = "the password is: qwerty";
+        String content = "password";
+        // place for your assertion using MatcherAssert.assertThat and proper matcher from Matchers.
+    }
+
+    @Test
+    public void assertCollectionHasSize5() {
+        Collection<Integer> numbers = Arrays.asList(42, 15, 21, 40, 13);
+        // place for your assertion using MatcherAssert.assertThat and proper matcher from Matchers.
+    }
+
+    @Test
+    public void assertPointsCollectionHasPointsAAndB() {
+        Point pointA = new Point(10, 20);
+        Point pointB = new Point(100, 200);
+        Point pointC = new Point(200, 100);
+        Point pointD = new Point(5, 0);
+        Collection<Point> points = Arrays.asList(pointA, pointB, pointC, pointD);
+        // place for your assertion using MatcherAssert.assertThat and proper matcher from Matchers.
+        // hint: the proper matcher refers to collection elements as items
+    }
+}
